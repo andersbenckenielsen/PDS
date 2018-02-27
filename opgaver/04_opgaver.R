ef4dc5d375ac5fa7b7abfef2a1a171f868e96f5b
andersbenckenielsen <andersbenckenielsen@github.com>
2018-02-27 17:16:42 +0100

    Opg 4
    Har løst til og med opgave 4

--- opgaver/04_opgaver.R
+++ opgaver/04_opgaver.R
@@ -22,12 +22,42 @@ seats <- read_csv("https://raw.githubusercontent.com/jespersvejgaard/PDS/master/
 # Bemærk: Bliver ae, ao og aa skrevet forkert? Så klik filer -> reopen with encoding -> vælg UTF-8 -> sæt flueben i "Set as default..." -> OK
 
 # 1. Eksekvér præamblen ovenfor 
+
 # 2. Skriv en funktion, som tager to vektorer som argumenter, summerer dem og returnerer resultatet. 
-# 3. Skriv en funktion, som tager en vektor som argument, og som beregner standardafvigelsen hvis n > 30, og ellers printer "N er under 30!" og afbryder (hint: `break`). Test med vektorerne `x`, `y`, `z` ovenfor.  
+new_function <- function(x,y) {
+  print(x + y)
+}
+
+
+# 3. Skriv en funktion, som tager en vektor som argument, 
+# og som beregner standardafvigelsen hvis n > 30, og ellers printer "N er under 30!" 
+# og afbryder (hint: `break`). Test med vektorerne `x`, `y`, `z` ovenfor.  
+sd_function <- function(x) {
+  if (length(x) < 30) {
+    print("N er under 30!")
+    break
+} else {
+  print(sd(x))
+}
+}
+
+sd_function(y)
+sd_function(x)
+sd_function(z)
+
 # 4. Skriv et for-loop, som looper igennem alle kolonnerne i dataframen `seats` og beregner partiernes gennemsnitlige antal mandater
+for (i in seq_along(seats)) {
+  print(mean(seats[[i]], na.rm = T))
+}
+
 # 5. Brug `lapply()`, `sapply()`, `vapply()` eller `map_dbl()` til at beregne det samme
+
+
+
 # 6. Skriv en funktion, som simulerer et terningekast (hint: `sample()`)
+
 # 7. Skriv et while-loop, som eksekverer funktionen terningekast indtil du har slået 3 seksere i streg. Hvor mange gange skal du slå, for at det sker?  
+
 # 8. Wrap dit while-loop i et for-loop, og slå 3 seksere i streg 100 gange. Hvor mange slags skal du bruge i gennemsnit? 
 
 
